b85c30db087eb52439f78b1d315426c76ca093db
Manzood <manzood.naqvi@gmail.com>
2020-03-28 15:39:11 +0530

    This updated solution for problem D now works!

--- Round-629/d.cpp
+++ Round-629/d.cpp
@@ -14,52 +14,74 @@ int main() {
 	while (t--) {
 		int n;
 		scanf("%d",&n);
-		vector <int> colours(n);
-		int current,previous;
-		scanf("%d",&previous);
-		int col=1;
-		int first=previous;
-		colours[0]=col;
-		int k=1;
+		vector <int> types(n);
+		vector <int> cols(n);
+		scanf("%d",&types[0]);
+		int changed=0;
+		int samepos=-1;
+		int previous=types[0];
 		for (int i=1;i<n;i++) {
-			scanf("%d",&current);
-			if (current!=previous && i<n-1) {
-				k=2;
-				if (col==1)
-					col=2;
-				else
-					col=1;
-				colours[i]=col;
+			scanf("%d",&types[i]);
+			if (types[i]!=types[0])
+				changed=1;
+			if (types[i]==previous) {
+				samepos=i-1;
 			}
-			else if (i==n-1) {
-				//debug(first);
-				//debug(previous);
-				if (first!=previous && current!=previous) {
-					k=3;
-					colours[i]=3;
+			previous=types[i];
+		}
+		if (samepos==-1) {
+			if (types[n-1]==types[0])
+				samepos=n-1;
+		}
+		if (changed==0) {
+			printf("1\n");
+			for (int i=0;i<n;i++) {
+				printf("1 ");
+			}
+		}
+		else if (n%2==0) {
+			printf("2\n");
+			for (int i=0;i<n;i++) {
+				if (i%2==0) {
+					printf("1 ");
 				}
-				else if (k==2) {
-					if (current!=previous) {
-						if (col==1)
-							colours[i]=2;
-						else
-							colours[i]=1;
+				else
+					printf("2 ");
+			}
+		}
+		else {
+			if (samepos==-1) {
+				printf("3\n");
+				for (int i=0;i<n;i++) {
+					if (i==n-1) {
+						printf("3 ");
 					}
-					else {
-						colours[i]=col;
+					else if (i%2==0) {
+						printf("1 ");
 					}
+					else
+						printf("2 ");
 				}
-				else 
-					colours[i]=col;
 			}
 			else {
-				colours[i]=col;
+				printf("2\n");
+				int current=1;
+				for (int i=0;i<n;i++) {
+					if (i==samepos) {
+						printf("%d ",current);
+					}
+					else {
+						//cout<<"Samepos="<<samepos<<endl;
+						printf("%d ",current);
+						if (current==1) {
+							current=2;
+						}
+						else {
+							current=1;
+						}
+					}
+				}
 			}
-			previous=current;
-		}
-		printf("%d\n",k);
-		for (int i=0;i<n;i++) {
-			printf("%d ",colours[i]);
 		}
 		printf("\n");
 	}
