c71bf532ffcc401ab7b19261e22fee23a08b6d35
Manzood <manzood.naqvi@gmail.com>
2020-05-26 14:50:21 +0530

    Working solution for F!

--- Round_644/f.cpp
+++ Round_644/f.cpp
@@ -12,84 +12,37 @@ int main() {
 		for (int i=0;i<n;i++) {
 			cin>>a[i];
 		}
-		int changes[2];
-		changes[0]=-1;
-		changes[1]=-1;
-		int count=0;
-		bool possible=true;
-		//if there are more than 2 unique changes, not possible
-		for (int i=1;i<n;i++) {
-			for (int j=0;j<m;j++) {
-				//up to two indexes where it is differentiated
-				if (a[0][j]!=a[i][j]) {
-					if (changes[count]==-1) {
-						changes[count]=j;
-					}
-					else {
-						if (changes[count]!=j) {
-							count++;
-							if (count==2) 
-								possible=false;
-							else 
-								changes[count]=j;
-						}
+		bool found=false;
+		string ans;
+		for (int i=0;i<m && !found;i++) {
+			int temp=a[0][i];
+			for (char j='a';j<='z' && !found;j++) {
+				a[0][i]=j;
+				//debug(a[0]);
+				bool possible=true;
+				for (int x=1;x<n;x++) {
+					int cnt=0;
+					//debug(a[0]);
+					//debug(a[x]);
+					for (int y=0;y<m;y++) {
+						if (a[0][y]!=a[x][y]) 
+							cnt++;
+						if (cnt>1)
+							possible=false;
 					}
 				}
+				//debug(possible);
+				if (possible) {
+					ans=a[0];
+					found=true;
+				}
 			}
+			a[0][i]=temp;
+			//debug(a[0]);
 		}
-		if (possible) {
-			count=0;
-			char letter='A';
-			for (int i=1;i<n && count==0;i++) {
-				if (letter=='A') {
-					if (a[0][changes[count]]!=a[i][changes[count]]) {
-						letter=a[i][changes[count]];
-					}
-				}
-				else {
-					if (a[i][changes[count]]!=letter) {
-						count++;
-					}
-				}
-			}
-			if (count==0) {
-				for (int i=0;i<m;i++) {
-					if (i==changes[0]) 
-						cout<<letter;
-					else 
-						cout<<a[0][i];
-				}
-				cout<<endl;
-				continue;
-			}
-			count=1;
-			letter='A';
-			for (int i=1;i<n && count==1;i++) {
-				if (letter=='A') {
-					if (a[0][changes[count]]!=a[i][changes[count]]) {
-						letter=a[i][changes[count]];
-					}
-				}
-				else {
-					if (a[i][changes[count]]!=letter) {
-						count++;
-						possible=false;
-					}
-				}
-			}
-			if (possible) {
-				for (int i=0;i<m;i++) {
-					if (i==changes[1] && letter!='A') 
-						cout<<letter;
-					else 
-						cout<<a[0][i];
-				}
-				cout<<endl;
-				continue;
-			}
-		}
-		printf("-1\n");
+		if (found) 
+			cout<<ans<<endl;
+		else
+			cout<<"-1"<<endl;
 	}
 }
-
-
