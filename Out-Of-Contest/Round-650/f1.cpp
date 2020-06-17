ef41b82fc750d2bbf1290508c897505c303f505b
Manzood <manzood.naqvi@gmail.com>
2020-06-17 21:20:45 +0530

    Just a temporary commit, not much useful here.

---
+++ Out-Of-Contest/Round-650/f1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+
+void swap (int *a, int *b) {
+	int temp=*b;
+	*b=*a;
+	*a=temp;
+}
+
+int main() {
+	
+}
