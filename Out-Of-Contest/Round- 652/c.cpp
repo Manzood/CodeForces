e1905d1ea71d1339ae4cacd4c1b4f7e5fe4d32e7
Manzood <manzood.naqvi@gmail.com>
2020-06-25 19:55:38 +0530

    Solved C

--- Out-Of-Contest/Round- 652/c.cpp
+++ Out-Of-Contest/Round- 652/c.cpp
@@ -18,20 +18,32 @@ int main() {
         for (int i=0;i<n;i++) {
             scanf("%d",&a[i]);
         }
+        int ones=0;
         for (int i=0;i<k;i++) {
             scanf("%d",&b[i]);
+            if (b[i]==1) {
+                ones++;
+            }
         }
         sort(a.begin(),a.end(),check);
         sort(b.begin(),b.end());
         long long ans=0;
-        int current=0;
         for (int i=0;i<k;i++) {
-            ans+=a[current];
-            //debug(ans);
-            current+=b[i]-1;
-            ans+=a[current];
-            //debug(ans);
-            current++;
+            ans+=a[i];
+            // debug(a[i]);
+        }
+        for (int i=0;i<ones;i++) {
+            ans+=a[i];
+            // debug(a[i]);
+        }
+        int current=k-1;
+        for (int i=0;i<k;i++) {
+            current+=(b[i]-1);
+            if (b[i]>1) {
+                ans+=a[current];
+                // debug(a[current]);
+                // debug(current);
+            }
         }
         cout<<ans<<endl;
     }
