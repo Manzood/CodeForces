6071b4d8a779acbb6c2ea227d901a9c676331b5c
Manzood <manzood.naqvi@gmail.com>
2020-06-26 22:30:48 +0530

    Solved D :)

--- Educational-Round-90/d.cpp
+++ Educational-Round-90/d.cpp
@@ -1,31 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define debug(x) cout<<#x<<" = "<<x<<endl;
+#define int long long
 
-int main() {
+int32_t main() {
 	int t;
 	cin>>t;
 	while (t--) {
 		int n;
-		scanf("%d",&n);
+		scanf("%lld",&n);
 		vector <int> a(n);
-		vector <int> evenpref;
-		vector <int> oddpref;
 		for (int i=0;i<n;i++) {
-			scanf("%d",&a[i]);
-			if (i%2==1 && i>0) {
-				if (i==1) {
-					evenpref.push_back(a[0]);
-					oddpref.push_back(a[1]);
-				}
-				else {
-					evenpref.push_back(evenpref[i/2-1]+a[i-1]);
-					oddpref.push_back(oddpref[i/2-1]+a[i]);
-				}
-			}
-		}
-		if (n%2==1) {
-			evenpref.push_back(evenpref[evenpref.size()-1]+a[n-1]);
+			scanf("%lld",&a[i]);
 		}
 		// each time odd pref is greater than even pref, store it
 		// each time even pref is greater, the previous subarray is already desirable for all future subarrrays
@@ -33,27 +19,21 @@ int main() {
 		int m=0;
 		int gain=0;
 		int sum=0;
-		int oddsum=0,evensum=0;
 		for (int i=0;i<n;i++) {
-			if (i%2==0) evensum+=a[i];
-			else oddsum+=a[i];
-			if (i%2==1 && oddsum>evensum) {
-					gain+=a[i]-a[i-1];
-					debug(gain);
+			if (gain<0) gain=0;
+			if (i%2==1) gain+=a[i]-a[i-1];
+			if (i%2==0) {
+				sum+=a[i];
 			}
-			else if (i%2==1 && oddsum<=evensum) {
-				evensum-=oddsum;
-				oddsum=0;
-				gain=0;
-			}
-			if (i%2==0) sum+=a[i];
 			m=max(gain,m);
 		}
-		debug(m);
-		m+=sum;
-		if (n%2==1) {
-			m+=a[n-1];
+		gain=0;
+		for (int i=n-1;i>=0;i--) {
+			if (gain<0) gain=0;
+			if (i%2==1 && i<n-1) gain+=a[i]-a[i+1];
+			m=max(gain,m);
 		}
-		printf("%d\n",m);
+		m+=sum;
+		printf("%lld\n",m);
 	}
 }
