10589eb8b6549075e778e2b21a19f4e8dc3e17e5
Manzood <manzood.naqvi@gmail.com>
2020-07-19 22:19:26 +0530

    Added an unsuccessful soln

---
+++ Out-Of-Contest/Round-657/a.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        scanf("%d\n", &n);
+        string s;
+        cin >> s;
+        string good = "abacaba";
+        bool found = false;
+        int count = 0;
+        for (int i=0; i<=n-7; i++) {
+            bool current = true;
+            for (int j=0; j<7; j++) {
+                if (s[i+j] != good[j])
+                    current = false;
+            }
+            if (current) {
+                found = true;
+                count++;
+            }
+        }
+        if (count == 0) {
+            for (int i=0; i<=n-7; i++) {
+                bool current = true;
+                for (int j=0; j<7; j++) {
+                    if (s[i+j] != '?' && s[i+j] != good[j]) 
+                        current = false;
+                }
+                if (current) {
+                    found = true;
+                    for (int j=0; j<7; j++) {
+                        if (s[i+j] == '?') s[i+j] = good[j];
+                    }
+                    break;
+                }
+            }
+        }
+        if (found && count < 2) {
+            printf("YES\n");
+            for (int i=0; i<n; i++) {
+                if (s[i] == '?') printf("z");
+                else printf("%c", s[i]);
+            }
+            printf("\n");
+        }
+        else printf("NO\n");
+    }
+}
