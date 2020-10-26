7e61fc7633aa8f97f993b86edd1e4d736e02aac7
Manzood <manzood.naqvi@gmail.com>
2020-10-26 23:34:57 +0530

    Added A small but incorrect change to C

--- Rounds/Round-678/c.cpp
+++ Rounds/Round-678/c.cpp
@@ -4,40 +4,42 @@ using namespace std;
 #define int long long
 const int mod = 1e9+7;
 
+// this is currently an incorrect soln
 int32_t main() {
     int n, x, pos;
     scanf("%lld%lld%lld", &n, &x, &pos);
     int remaining = n;
-    bool flag = true;
     long long ans = 1;
     int numgreater = n-x;
     int numless = x-1;
     int left = 0, right = n;
     int mid = (left + right) / 2;
-    while (flag && left < right) {
+    vector <int> calc(n, -1);
+    bool found = false;
+    while (left < right) {
         mid = (left + right) / 2;
-        if (pos > mid) {
-            ans *= numless;
-            numless--;
-            left = mid + 1;
-        }
-        else if (pos < mid) {
-            ans *= numgreater;
-            numgreater--;
+        if (mid < pos) {
+            if (!found) calc[mid-1] = numgreater;
+            left = mid+1;
+        } else if (mid > pos) {
+            if (!found) calc[mid-1] = numless;
             right = mid;
+        } else {
+            calc[mid-1] = 1;
+            left = mid+1;
+            found = true;
+            remaining--;
         }
-        else {
-            flag = false;
-        }
-        ans %= mod;
-        if (ans == 0) {
-            flag = false;
-            break;
-        }
-        remaining--;
+        if (!found) remaining--;
     }
-    for (int i = remaining; i > 1; i--) {
-        ans *= i;
+    if (!(left > 0 && left - 1 == pos))
+        ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (calc[i] == -1) {
+            ans *= remaining--;
+        } else {
+            ans *= calc[i];
+        }
         ans %= mod;
     }
     printf("%lld\n", ans);
