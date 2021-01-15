e429310c54e615f4feb592317c3189f5d9f26f51
Manzood <manzood.naqvi@gmail.com>
2021-01-15 21:49:04 +0530

    Improved Segment tree, Solved D

--- Educational/Educational-Round-102/D/D.cpp
+++ Educational/Educational-Round-102/D/D.cpp
@@ -28,11 +28,13 @@ void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
 
 struct segtree {
     int size;
+    vector <long long> sum;
     vector <long long> m;
     vector <long long> mx;
-    void init (int n) { // making our size a power of two
+    void init (int n) { // just making our size a power of two
         size = 1;
         while (size < n) size *= 2;
+        sum.assign(2*size-1, 0);
         m.assign(2*size-1, inf);
         mx.assign(2*size-1, -inf);
     }
@@ -42,24 +44,28 @@ struct segtree {
         int counter = 0;
         for (int i = 2*size-2; i >= 0; i--) {
             if (counter++ < numzeroes) {
+                sum[i] = 0;
                 m[i] = inf;
                 mx[i] = -inf;
             }
             else if (n > 0) {
-                m[i] = a[--n];
-                mx[i] = a[n + 1];
+                sum[i] = a[--n];
+                m[i] = a[n];
+                mx[i] = a[n];
             }
             else {
-                mx[i] = max(mx[2*i+1], mx[2*i+2]);
+                sum[i] += sum[2*i+1] + sum[2*i+2];
                 m[i] = min(m[2*i+1], m[2*i+2]);
+                mx[i] = max(mx[2*i+1], mx[2*i+2]);
             }
         }
     }
-    void set (int i, int v) {
+    void set (int i, int v) { // setting index i to element v
         set (i, v, 0, 0, size);
     }
     void set (int i, int v, int x, int lx, int rx) {
         if (rx - lx == 1) {
+            sum[x] = v;
             m[x] = v;
             mx[x] = v;
             return;
@@ -70,11 +76,23 @@ struct segtree {
         } else {
             set(i, v, 2 * x + 2, mid, rx);
         }
+        sum[x] = sum[2 * x + 1] + sum[2 * x + 2];
         m[x] = min(m[2 * x + 1], m[2 * x + 2]);
         mx[x] = max(mx[2 * x + 1], mx[2 * x + 2]);
         return;
     }
-    long long getmin (int left, int right) {
+    long long getsum (int left, int right) { // returns sum between indexes [left, right - 1], inclusive
+        return getsum(left, right, 0, 0, size);
+    }
+    long long getsum (int left, int right, int x, int lx, int rx) {
+        if (lx >= right || left >= rx) return 0;
+        if (lx >= left && rx <= right) return sum[x];
+        int mid = (lx + rx) / 2;
+        long long s1 = getsum (left, right, 2 * x + 1, lx, mid);
+        long long s2 = getsum (left, right, 2 * x + 2, mid, rx);
+        return s1 + s2;
+    }
+    long long getmin (int left, int right) { // returns min between indexes [left, right - 1], inclusive
         return getmin(left, right, 0, 0, size);
     }
     long long getmin (int left, int right, int x, int lx, int rx) {
@@ -85,7 +103,7 @@ struct segtree {
         long long s2 = getmin (left, right, 2 * x + 2, mid, rx);
         return min(s1, s2);
     }
-    long long getmax (int left, int right) {
+    long long getmax (int left, int right) { // returns max between indexes [left, right - 1], inclusive
         return getmax (left, right, 0, 0, size);
     }
     long long getmax (int left, int right, int x, int lx, int rx) {
@@ -94,7 +112,7 @@ struct segtree {
         int mid = (lx + rx) / 2;
         long long s1 = getmax (left, right, 2 * x + 1, lx, mid);
         long long s2 = getmax (left, right, 2 * x + 2, mid, rx);
-        return max (s1, s2);
+        return max(s1, s2);
     }
 };
 
@@ -113,10 +131,6 @@ int32_t main() {
             else a[i] = -1;
             pref[i+1] = pref[i] + a[i];
         }
-        for (auto x: pref) {
-            printf("%lld ", x);
-        }
-        printf("\n");
         segtree st;
         st.init(n+1);
         st.build(pref);
@@ -125,28 +139,23 @@ int32_t main() {
             scanf("%lld%lld", &left, &right);
             left--;
             right--;
-            // redo segtree, then check ans
-            int mi = inf, mx = 0;
-            int leftmin = st.getmin (0, left);
-            int leftmax = st.getmax (0, left );
-            int rightmin = st.getmin (right+1, n);
-            int rightmax = st.getmax (right+1, n);
-            if (leftmin == inf) leftmin = 0;
+            int leftmin = st.getmin (0, left + 1);
+            int leftmax = st.getmax (0, left + 1);
+            int rightmin = st.getmin (right + 2, n + 1);
+            int rightmax = st.getmax (right + 2, n + 1);
             if (rightmin == inf) rightmin = 0;
-            if (leftmax == -inf) leftmax = 0;
+            if (leftmin == inf) leftmin = 0;
             if (rightmax == -inf) rightmax = 0;
-            debug(leftmin, leftmax);
-            mi = leftmin;
-            mx = leftmax;
-            int val = pref[right+1];
-            if (right == n - 1) val = 0;
-            rightmax -= val; rightmin -= val;
-            rightmax = max (0LL, rightmax);
-            rightmin = min (0LL, rightmin);
-            debug(rightmin, rightmax);
-            mi += rightmin;
-            mx += rightmax;
-            debug(mx, mi);
+            if (leftmax == -inf) leftmax = 0;
+            int val = pref[right + 1];
+            int fin = pref[left];
+            if (right + 1 == n) val = 0;
+            rightmax -= val;
+            rightmin -= val;
+            rightmax += fin;
+            rightmin += fin;
+            int mi = min(leftmin, rightmin);
+            int mx = max(leftmax, rightmax);
             int ans = mx - mi + 1;
             printf("%lld\n", ans);
         }
