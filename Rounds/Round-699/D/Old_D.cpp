2865d872bc02e6e9ccf8c11f2612e6eb3313a046
Manzood <manzood.naqvi@gmail.com>
2021-02-08 20:42:32 +0530

    Solved D

--- Rounds/Round-699/D/Old_D.cpp
+++ Rounds/Round-699/D/Old_D.cpp
@@ -3,40 +3,6 @@ using namespace std;
 #define debug(x) cout << #x << " = " << x << endl;
 #define int long long
 
-void not_cycle (int a, int b, int c, int m) {
-    vector <int> temp = {a, b, c};
-    int dir = 1;
-    int ind = 0;
-    int cnt = 0;
-    while (true) {
-        printf("%lld ", temp[ind]);
-        cnt++;
-        if (cnt == m + 1) break;
-        if (ind == 2) dir = -1;
-        if (ind == 0) dir = 1;
-        ind += dir;
-    }
-}
-
-void cycle (int a, int b, int c, int m) {
-    // is not actually a cycle
-    bool flag = true;
-    int cnt = 0;
-    for (int i = 0; i < m + 1; i++) {
-        if (flag) {
-            printf("%lld ", a);
-        } else {
-            if (cnt % 2 == 0) {
-                printf("%lld ", b);
-            } else {
-                printf("%lld ", c);
-            }
-            cnt++;
-        }
-        flag ^= 1;
-    }
-}
-
 int32_t main() {
     int t;
     cin >> t;
@@ -76,24 +42,21 @@ int32_t main() {
                     }
                     printf("\n");
                 } else {
+                    vector <int> temp = {1, 2, 3};
+                    int ind = 0;
+                    int dir = 1;
                     if (a[0][1] == a[1][2]) {
-                        if ((m / 2) % 2 == 1) {
-                            not_cycle(1, 2, 3, m);
-                        } else {
-                            cycle (2, 3, 1, m);
-                        }
+                        ind = 0;
                     } else if (a[1][2] == a[2][0]) {
-                        if ((m / 2) % 2 == 1) {
-                            not_cycle (2, 3, 1, m);
-                        } else {
-                            cycle (3, 1, 2, m);
-                        }
+                        temp = {2, 3, 1};
                     } else {
-                        if ((m / 2) % 2 == 1) {
-                            not_cycle (3, 1, 2, m);
-                        } else {
-                            cycle (1, 2, 3, m);
-                        }
+                        temp = {3, 1, 2};
+                    }
+                    for (int i = 0; i < m + 1; i++) {
+                        printf("%lld ", temp[ind]);
+                        if (ind == 2) dir = -1;
+                        if (ind == 0) dir = 1;
+                        ind += dir;
                     }
                     printf("\n");
                 }
@@ -107,3 +70,4 @@ int32_t main() {
         }
     }
 }
+
