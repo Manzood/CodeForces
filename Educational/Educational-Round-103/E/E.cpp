5a36224186506b47c65bb2fb106acc37ab765f37
Manzood <manzood.naqvi@gmail.com>
2021-02-09 16:05:59 +0530

    Tried E

--- Educational/Educational-Round-103/E/E.cpp
+++ Educational/Educational-Round-103/E/E.cpp
@@ -3,27 +3,22 @@ using namespace std;
 #define debug(x) cout << #x << " = " << x << endl;
 #define int long long
 
-bool check (string& p, string &s) {
-    for (int i = 0; i < (int) s.size(); i++) {
-        if (p[i] == '_') continue;
-        if (p[i] != s[i]) return false;
-    }
-    return true;
-}
-
 int32_t main() {
     int n, m, k;
     scanf("%lld%lld%lld", &n, &m, &k);
-    vector <string> patterns(n);
+    map <string, int> patterns;
     vector <string> s(m);
     vector <vector <int>> adj(n);
+    vector <int> pos(n);
     for (int i = 0; i < n; i++) {
-        cin >> patterns[i];
+        string temp;
+        cin >> temp;
+        patterns[temp] = i + 1;
     }
     for (int i = 0; i < m; i++) {
         cin >> s[i];
+        scanf("%lld", &pos[i]);
     }
     for (int i = 0; i < n; i++) {
-
     }
 }
