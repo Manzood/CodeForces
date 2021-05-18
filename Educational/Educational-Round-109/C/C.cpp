5c78bbd37dbb8161c66817876e6a6898454f8e3b
Manzood <manzood.naqvi@gmail.com>
2021-05-18 17:29:13 +0530

    Solved C

--- Educational/Educational-Round-109/C/C.cpp
+++ Educational/Educational-Round-109/C/C.cpp
@@ -16,35 +16,73 @@ int32_t main() {
         for (int i = 0; i < n; i++) {
             cin >> a[i].second;
         }
-        vector <int> temp;
+        vector <int> temp(n);
         iota (temp.begin(), temp.end(), 0);
         sort (temp.begin(), temp.end(), [&a](int x, int y) {
             return a[x].first < a[y].first;
         });
         vector <int> ans (n, -1);
-        vector <int> parity[2][2]; // parity[0][0] stores left, even, parity[0][1] stores left, odd
+        // store the indexes
+        vector <int> st[2][2];
+        // st[0][0] stores {left, even}, st[1][1] stores {right, odd}
         for (int i = 0; i < n; i++) {
-            // first make non-reflective comparisons
-            int ind = temp[i];
-            int p = a[i].first % 2;
-            int l;
-            if (a[ind].second == 'L') {
-                l = 0;
-                if ((int) parity[1][p].size() == 0) {
-                    // add it to the stack
-                    parity[0][p].push_back (a[temp[i]].first);
+            // get rid of left, first
+            int p = a[temp[i]].first % 2;
+            if (a[temp[i]].second == 'L') {
+                if (st[1][p].size() == 0) {
+                    st[0][p].push_back (temp[i]);
+                    // debug(a[temp[i]].first);
                 } else {
-                    // otherwise pop
-                    // ans[temp[i]] = abs (parity[1][0].back() - );
-                    int tempsize = parity[1][0].size();
-                    ans[temp[1]] = abs (parity[1][0][tempsize-1] - a[temp[i]].first);
-                    parity[1][0].pop_back();
+                    ans[temp[i]] = (abs (a[temp[i]].first - a[st[1][p][st[1][p].size()-1]].first)) / 2;
+                    ans[st[1][p][st[1][p].size()-1]] = (abs (a[temp[i]].first - a[st[1][p][st[1][p].size()-1]].first)) / 2;
+                    st[1][p].pop_back();
                 }
             } else {
-                l = 1;
-                // add it to the stack
-                parity[1][0].push_back (a[temp[i]].first);
+                st[1][p].push_back (temp[i]);
             }
         }
+        // get rid of left and right that can still collide
+        // collides with other stuff, right within its own stack
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (i == 0) reverse (st[i][j].begin(), st[i][j].end());
+                while (st[i][j].size() >= 2) {
+                    int sz = st[i][j].size();
+                    if (i == 1) {
+                        ans[st[i][j][sz-1]] = (m - a[st[i][j][sz-1]].first + m - a[st[i][j][sz-2]].first) / 2;
+                        ans[st[i][j][sz-2]] = (m - a[st[i][j][sz-1]].first + m - a[st[i][j][sz-2]].first) / 2;
+                        st[i][j].pop_back();
+                        st[i][j].pop_back();
+                    } else {
+                        ans[st[i][j][sz-1]] = (a[st[i][j][sz-1]].first + a[st[i][j][sz-2]].first) / 2;
+                        ans[st[i][j][sz-2]] = (a[st[i][j][sz-1]].first + a[st[i][j][sz-2]].first) / 2;
+                        st[i][j].pop_back();
+                        st[i][j].pop_back();
+                    }
+                }
+            }
+        }
+        // for (int i = 0; i < (int) st[0][0].size(); i++) {
+            // if (st[1][0].size() == 0) break;
+            // ans[st[0][0][st[0][0].size()-1]] = (abs (a[st[0][0][st[0][0].size()-1]].first - a[st[1][0][st[1][0].size()-1]].first)) / 2;
+            // st[1][0].pop_back();
+        // }
+        // for (int i = 0; i < (int) st[0][1].size(); i++) {
+            // if (st[1][1].size() == 0) break;
+            // ans[st[0][1][st[0][1].size()-1]] = (abs (a[st[0][1][st[0][1].size()-1]].first - a[st[1][1][st[1][1].size()-1]].first)) / 2;
+            // st[1][1].pop_back();
+        // }
+        if (st[0][0].size() > 0 && st[1][0].size() > 0) {
+            ans[st[0][0][0]] = (a[st[0][0][0]].first + m - a[st[1][0][0]].first + m) / 2;
+            ans[st[1][0][0]] = (a[st[0][0][0]].first + m - a[st[1][0][0]].first + m) / 2;
+        }
+        if (st[0][1].size() > 0 && st[1][1].size() > 0) {
+            ans[st[0][1][0]] = (a[st[0][1][0]].first + m - a[st[1][1][0]].first + m) / 2;
+            ans[st[1][1][0]] = (a[st[0][1][0]].first + m - a[st[1][1][0]].first + m) / 2;
+        }
+        for (int i = 0; i < n; i++) {
+            printf("%lld ", ans[i]);
+        }
+        printf("\n");
     }
 }
---
+++ Educational/Educational-Round-109/C/C_old.cpp
@@ -0,0 +1,61 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define debug(x) cout << #x << " = " << x << endl;
+#define int long long
+
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        scanf("%lld%lld", &n, &m);
+        vector <pair <int, char>> a(n);
+        for (int i = 0; i < n; i++) {
+            scanf("%lld", &a[i].first);
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> a[i].second;
+        }
+        vector <int> temp;
+        iota (temp.begin(), temp.end(), 0);
+        sort (temp.begin(), temp.end(), [&a](int x, int y) {
+            return a[x].first < a[y].first;
+        });
+        vector <int> ans (n, -1);
+        vector <int> parity[2][2]; // parity[0][0] stores left, even, parity[0][1] stores left, odd
+        for (int i = 0; i < n; i++) {
+            // first make non-reflective comparisons
+            int ind = temp[i];
+            int p = a[i].first % 2;
+            int l;
+            if (a[ind].second == 'L') {
+                l = 0;
+                if ((int) parity[1][p].size() == 0) {
+                    // add it to the stack
+                    parity[0][p].push_back (a[temp[i]].first);
+                } else {
+                    // otherwise pop
+                    // ans[temp[i]] = abs (parity[1][0].back() - );
+                    int tempsize = parity[1][0].size();
+                    ans[temp[i]] = abs (parity[1][0][tempsize-1] - a[temp[i]].first);
+                    ans[temp[i]] = abs (parity[1][0][tempsize-1] - a[temp[i]].first);
+                    parity[1][0].pop_back();
+                }
+            } else {
+                l = 1;
+                // add it to the stack
+                parity[1][0].push_back (a[temp[i]].first);
+            }
+        }
+        // now, see if the left and right ones collide
+        for (int i = 0; i < parity[0][0].size(); i++) {
+            if (parity[1][0].size() == 0) {
+                break;
+            }
+
+        }
+        for (int i = 0; i < parity[0][1].size(); i++) {
+
+        }
+    }
+}
