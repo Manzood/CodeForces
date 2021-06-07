d730f192172484f5113eff4537b9043a510d41bd
Manzood <manzood.naqvi@gmail.com>
2021-06-07 17:22:41 +0530

    Solved E

--- Educational/Educational-Round-110/E/E.cpp
+++ Educational/Educational-Round-110/E/E.cpp
@@ -4,61 +4,59 @@ using namespace std;
 #define int long long
 
 const int LOG = 20;
+const int N = 3e5 + 5;
 
 int32_t main() {
     // keep track of ancestors
     // efficiently find ancestors using binary lifting
     // keep track of children so as to be able to move downwards
-    // keep track of each node. The question uses weird numbering
-    int q, a0, c0;
-    cin >> q >> a0 >> c0;
-    vector <vector <int>> ancestor;
-    ancestor.push_back (vector <int> (LOG, 0));
-    vector <pair <int, int>> vertices = {{a0, c0}};
-    vector <int> vertex = {0};
-    vector <int> child = {-1};
+    int q;
+    vector <pair <int, int>> vertices(N);
+    cin >> q >> vertices[0].first >> vertices[0].second;
+    vector <vector <int>> ancestor(N);
+    ancestor[0] = vector <int> (LOG, -1);
+    // vector <int> child(N, -1);
+
     for (int i = 1; i <= q; i++) {
-        vertex.push_back (-1);
         int query; cin >> query;
+
         if (query == 1) {
-            int p, a, c;
-            cin >> p >> a >> c;
-            p = vertex[p];
-            int v = vertices.size();
-            vertices.push_back ({a, c});
-            vertex[i] = vertices.size() - 1;
-            child[p] = v;
-            child.push_back (-1);
-            ancestor.push_back (vector <int> (LOG, 0));
-            ancestor[v][0] = p;
+            int p;
+            cin >> p >> vertices[i].first >> vertices[i].second;
+            // child[p] = i;
+            ancestor[i] = vector <int> (LOG, -1);
+            ancestor[i][0] = p;
+
             for (int j = 1; j < LOG; j++) {
-                ancestor[v][j] = ancestor[ancestor[v][j-1]][j-1];
+                if (ancestor[i][j-1] >= 0) ancestor[i][j] = ancestor[ancestor[i][j-1]][j-1];
             }
+
         } else {
             int v, w;
             cin >> v >> w;
-            v = vertex[v];
             int original = v;
-            // find the highest ancestor above v;
-            int k = LOG - 1;
-            while (k >= 0) {
-                if (vertices[ancestor[v][k]].first > 0) {
-                    v = ancestor[v][k];
-                }
-                k--;
-            }
             int taken = 0;
             int cost = 0;
+
             while (taken < w && v != -1) {
                 // debug (v);
+                v = original;
+                int k = LOG - 1;
+                while (k >= 0) {
+                    if (ancestor[v][k] >= 0 && vertices[ancestor[v][k]].first > 0) {
+                        v = ancestor[v][k];
+                    }
+                    k--;
+                }
+
                 int rem = w - taken;
                 taken += min (vertices[v].first, rem);
-                // cost is wrong??
                 cost += min (vertices[v].first, rem) * vertices[v].second;
                 vertices[v].first = max (0LL, vertices[v].first - rem);
                 if (v == original) break;
-                v = child[v];
+                // v = child[v];
             }
+
             printf("%lld %lld\n", taken, cost);
         }
     }
