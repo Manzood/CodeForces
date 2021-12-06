67b0c37be6baf897e1c0ae3f6fb1d9444184c7aa
Manzood <manzood.naqvi@gmail.com>
2021-12-06 18:08:40 +0530

    Updated random file

--- Random/PermCheck.cpp
+++ Random/PermCheck.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
+using namespace std;
 // you can use includes, for example:
 // #include <algorithm>
 
@@ -6,26 +7,20 @@
 // cout << "this is a debug message" << endl;
 
 int solution(vector<int> &A) {
-    int n=A.size();
+    int n = (int)A.size();
     int counter[100000];
-    int max=0;
-    if (n==1)
-        if(A[0]!=1)
-            return 0;
-    for(long long i=0;i<100000;i++)
-    {
-        counter[i]=0;
+    int max = 0;
+    if (n == 1)
+        if (A[0] != 1) return 0;
+    for (long long i = 0; i < 100000; i++) {
+        counter[i] = 0;
     }
-    for (int i=0;i<n;i++)
-    {
-        counter[A[i]-1]++;
-        if (A[i]>max)
-            max=A[i];
+    for (int i = 0; i < n; i++) {
+        counter[A[i] - 1]++;
+        if (A[i] > max) max = A[i];
     }
-    for(long long i=0;i<max;i++)
-    {
-        if(counter[i]!=1)
-            return 0;
+    for (long long i = 0; i < max; i++) {
+        if (counter[i] != 1) return 0;
     }
     return 1;
 }
