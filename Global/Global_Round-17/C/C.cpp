c8aa5134f3ebfd7744d85bac87736621a5bf49e3
Manzood <manzood.naqvi@gmail.com>
2022-01-03 01:44:48 +0530

    Solved C

--- Global/Global_Round-17/C/C.cpp
+++ Global/Global_Round-17/C/C.cpp
@@ -1,16 +1,13 @@
 #include "bits/stdc++.h"
-
 #ifdef local
     #include "custom/debugger.h"
 #else
     #define debug(x) 42;
 #endif
-
 using namespace std;
 #define int long long
 
-template <class T>
-using min_heap = priority_queue<T, vector<T>, greater<T> >;
+// binary search
 
 int32_t main () {
     int t;
@@ -23,45 +20,29 @@ int32_t main () {
             scanf("%lld%lld", &a[i], &b[i]);
         }
 
-        min_heap<pair <int, int>> pq;
+        int low = 0;
+        int high = n;
         int ans = 1;
-        pq.push ({a[0], a[0]});
-
-        debug (pq.size());
-        debug (pq.top());
-        for (int i = 1; i < n; i++) {
-            pair <int, int> top_val = pq.top();
-
-            while (pq.size() > 0 && top_val.first <= 0) {
-                debug ("popping", pq.top());
-                pq.pop();
-                // sz--;
-                top_val = pq.top();
-                if (pq.size() > 0) {
-                    auto new_top = pq.top();
-                    new_top.first = new_top.first - (int) pq.size() + 1;
-                    pq.pop();
-                    pq.push(new_top);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // check if you can get mid people
+            int got = 0;
+            bool found = false;
+            for (int i = 0; i < n; i++) {
+                if (mid - got - 1 <= a[i] && got <= b[i]) {
+                    got++;
                 }
-                debug (pq.size(), pq.top());
-            }
-
-            if (b[i] >= (int) pq.size()) {
-                if (pq.size() > 0) {
-                    top_val = pq.top();
-                    pq.pop();
-                    top_val.first--;
-                    pq.push(top_val);
+                if (got == mid) {
+                    found = true;
+                    break;
                 }
-                pq.push({a[i], a[i]});
-
-                // inserting new value
-                // sz++;
-                ans = max ((int)pq.size(), ans);
             }
-            debug (i, pq.size());
-            debug (pq.top())
-            ans = max ((int) pq.size(), ans);
+            if (found) {
+                ans = max(mid, ans);
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
         printf("%lld\n", ans);
     }
