e9b5188e9b0da572123fd805b920da1c3e229e80
Manzood <manzood.naqvi@gmail.com>
2022-01-26 23:42:32 +0530

    Finally solved C. Been stuck on it for 2 weeks

--- Rounds/Round-765/C/C.cpp
+++ Rounds/Round-765/C/C.cpp
@@ -8,50 +8,80 @@
 
 using namespace std;
 #define int long long
-const int inf = 1e18;
 
 void solve() {
-    int n, l, target;
-    scanf("%lld%lld%lld", &n, &l, &target);
-    vector<int> pos(n);
-    vector<int> limit(n);
-    vector<int> influence(n, 0);
+    int n, l, k;
+    scanf("%lld%lld%lld", &n, &l, &k);
+    vector <int> a(n), influence(n, 0);
+    vector <int> limit(n);
     for (int i = 0; i < n; i++) {
-        scanf("%lld", &pos[i]);
+        scanf("%lld", &a[i]);
     }
     for (int i = 0; i < n; i++) {
         scanf("%lld", &limit[i]);
     }
-    pos.push_back(l);
+    a.push_back(l);
     for (int i = 0; i < n; i++) {
-        influence[i] = pos[i + 1] - pos[i];
+        influence[i] = a[i+1] - a[i];
     }
     int ans = 0;
-    for (int i = 0; i < n; i++) {
-        ans += limit[i] * influence[i];
+    vector <int> pref(n+1, 0);
+    vector <int> total_influence(n+1, 0);
+    // for (int i = n - 1; i >= 0; i--) {
+    //     total_influence[i] += influence[i];
+    //     if (i < n - 1) total_influence[i] += total_influence[i+1];
+    // }
+    for (int i = 0; i <= n; i++) {
+        if (i < n) ans += limit[i] * influence[i];
+        if (i > 0) pref[i] += pref[i-1] + limit[i-1] * influence[i-1];
+        if (i > 0) total_influence[i] += total_influence[i-1] + influence[i-1];
     }
-    debug (ans);
-    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(target + 1, vector<int>(target + 1, ans)));
-
-    for (int i = 1; i < n; i++) {
-        // keeping sign i
-        for (int j = 0; j <= target && j <= i - 1; j++) {
-            // removing the previous j positions
-            // int cur = limit[i-j-1];
-            int lastsign = i - j - 1;
-            int rem = target - j;
-            for (int k = 0; k + j <= target; k++) {
-                debug(i, j, k);
-                debug (rem, lastsign);
-                if (j == 0) dp[i][j][k] = min (dp[i][j][k], dp[lastsign][rem][k]);
-                if (j < target) dp[i][j][k] = min (dp[i][j][k], dp[i-1][j+1][k] - (influence[i-1] * limit[i-1]) + (influence[i-1] * limit[lastsign]));
-                ans = min(ans, dp[i][j][k]);
+    // debug (ans);
+    // int dp[n+1][k+1][k+1];
+    int best[n+1][k+1];
+    for (int i = 0; i < n+1; i++) {
+        for (int j = 0; j <= k; j++) {
+            // for (int x = 0; x <= k; x++) {
+            //     dp[i][j][x] = ans;
+            // }
+            best[i][j] = ans;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= k && j < i; j++) {
+            for (int x = 0; x <= k && x <= j; x++) {
+                // taking x in a row
+                // calculate improvement for those segments
+                int val = pref[i] - pref[i-x-1];
+                int new_val = limit[i-x-1] * (total_influence[i] - total_influence[i-x-1]);
+                int change = new_val - val;
+                // debug (i, x, val);
+                // debug (i, x, new_val);
+                best[i][j] = min(best[i][j], best[i-x-1][j-x] + change);
+                // best[i][j] = min(best[i][j], dp[i][j][x]);
             }
         }
     }
-
-    debug (dp);
-
+    // for (int i = 0; i <= n; i++) {
+    //     for (int j = 0; j <= k; j++) {
+    //         printf("%lld ", best[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // for (int i = 0; i <= n; i++) {
+    //     printf("i: %lld\n", i);
+    //     for (int j = 0; j <= k; j++) {
+    //         printf("j: %lld\n", j);
+    //         for (int x = 0; x <= k; x++) {
+    //             printf("%lld ", dp[i][j][x]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     printf("\n");
+    // }
+    for (int i = 0; i <= k; i++) {
+        ans = min(ans, best[n][i]);
+    }
     printf("%lld\n", ans);
 }
 
