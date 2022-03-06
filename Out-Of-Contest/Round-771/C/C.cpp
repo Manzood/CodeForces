bc0cd4c24084b1dd8fc0052e0426f246c2ea6caa
Manzood <manzood.naqvi@gmail.com>
2022-03-06 11:11:24 +0530

    Solved C

--- Out-Of-Contest/Round-771/C/C.cpp
+++ Out-Of-Contest/Round-771/C/C.cpp
@@ -10,7 +10,26 @@ using namespace std;
 #define int long long
 
 void solve([[maybe_unused]] int test) {
-
+    int n;
+    scanf("%lld", &n);
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &a[i]);
+    }
+    vector <int> smallest(n), biggest(n);
+    for (int i = 0; i < n; i++) {
+        biggest[i] = i == 0 ? a[i] : max(biggest[i-1], a[i]);
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        smallest[i] = i == n - 1 ? a[i] : min(smallest[i+1], a[i]);
+    }
+    int ans = 1;
+    for (int i = n - 2; i >= 0; i--) {
+        if (smallest[i] < smallest[i+1] && biggest[i] <= smallest[i+1]) {
+            ans++;
+        }
+    }
+    printf("%lld\n", ans);
 }
 
 int32_t main() {
