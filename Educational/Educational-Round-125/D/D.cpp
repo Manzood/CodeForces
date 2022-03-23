91197c960b16a1eb5f2d4c7e38c151dc24d42968
Manzood <manzood.naqvi@gmail.com>
2022-03-23 23:26:03 +0530

    Solved D

--- Educational/Educational-Round-125/D/D.cpp
+++ Educational/Educational-Round-125/D/D.cpp
@@ -13,31 +13,48 @@ const int inf = (int) 1e18;
 void solve([[maybe_unused]] int test) {
     int n, C;
     scanf("%lld%lld", &n, &C);
-    vector <int> c, d, h;
+    vector <int> c(n), d(n), h(n);
     vector <vector <int>> prod;
     for (int i = 0; i < n; i++) {
         scanf("%lld%lld%lld", &c[i], &d[i], &h[i]);
         prod.push_back({h[i] * d[i], c[i]});
     }
-    sort (prod.begin(), prod.end());
-    vector <int> min_suff(n, inf);
-    for (int i = n - 1; i >= 0; i--) {
-        min_suff[i] = min(prod[i][1], min_suff[i]);
+    vector <int> temp(C + 1, 0);
+    for (int i = 0; i < n; i++) {
+        temp[c[i]] = max(temp[c[i]], d[i] * h[i]);
+    }
+    // debug(temp);
+    vector <int> dp(C + 1, 0);
+    for (int i = 1; i <= C; i++) {
+        // if (temp[i] > 0) {
+            for (int j = 1; i * j <= C; j++) {
+                dp[i * j] = max(dp[i * j], temp[i] * j);
+            }
+        // }
+    }
+    for (int i = 1; i <= C; i++) {
+        dp[i] = max(dp[i], dp[i - 1]);
     }
     int m;
     scanf("%lld", &m);
-    vector <int> D, H;
+    vector <int> D(m), H(m);
+    vector <int> ans(m, -1);
     for (int i = 0; i < m; i++) {
         scanf("%lld%lld", &D[i], &H[i]);
+        int val = D[i] * H[i];
+        int ind = upper_bound(dp.begin(), dp.end(), val) - dp.begin();
+        if (ind != C + 1) {
+            ans[i] = ind;
+        }
     }
-    vector <int> ans(m, -1);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
+        printf("%lld%c", ans[i], " \n"[i == m - 1]);
     }
 }
 
 int32_t main() {
     int t = 1;
-    cin >> t;
+    // cin >> t;
     for (int tt = 1; tt <= t; tt++) {
         solve(tt);
     }
