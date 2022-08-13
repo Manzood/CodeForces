584ee1b2fe5a9c836a2da060d2bbb212a6c1f177
Manzood <manzood.naqvi@gmail.com>
2022-08-13 15:19:16 +0530

    Solved D

--- Rounds/Round-812/D/D.cpp
+++ Rounds/Round-812/D/D.cpp
@@ -9,8 +9,67 @@
 using namespace std;
 #define int long long
 
-void solve([[maybe_unused]] int test) {
+int query(int a, int b) {
+    cout << "? " << a << " " << b << endl;
+    int response;
+    cin >> response;
+    return response;
+}
 
+int solve_four(vector<int>& a) {
+    int winner = a[0];
+    int response = query(a[0], a[2]);
+    if (response == 0) {
+        response = query(a[1], a[3]);
+        if (response == 1)
+            winner = a[1];
+        else
+            winner = a[3];
+    } else if (response == 1) {
+        response = query(a[0], a[3]);
+        if (response == 1)
+            winner = a[0];
+        else
+            winner = a[3];
+    } else {
+        response = query(a[2], a[1]);
+        if (response == 1)
+            winner = a[2];
+        else
+            winner = a[1];
+    }
+    return winner;
+}
+
+void solve([[maybe_unused]] int test) {
+    int n;
+    cin >> n;
+    if (n == 1) {
+        int response = query(1, 2);
+        cout << "! " << response << endl;
+    } else {
+        queue<int> q;
+        for (int i = 0; i < (1LL << n); i++) {
+            q.push(i + 1);
+        }
+        while ((int)q.size() >= 4) {
+            vector<int> a(4);
+            for (int i = 0; i < 4; i++) {
+                a[i] = q.front();
+                q.pop();
+            }
+            int winner = solve_four(a);
+            q.push(winner);
+        }
+        int winner = q.front();
+        if ((int)q.size() == 2) {
+            int a = q.front();
+            q.pop();
+            int b = q.front();
+            winner = query(a, b) == 1 ? a : b;
+        }
+        cout << "! " << winner << endl;
+    }
 }
 
 int32_t main() {
