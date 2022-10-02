057470a84c4801092bdc51521772f4a1006009aa
Manzood <manzood.naqvi@gmail.com>
2022-10-03 01:10:06 +0530

    Added CodeForces Round 824

---
+++ Rounds/Round-824/B/B.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+
+#ifdef local
+#include "custom/debugger.h"
+#else
+#define debug(...) 42;
+#endif
+
+using namespace std;
+#define int long long
+
+void solve([[maybe_unused]] int test) {
+    int n;
+    scanf("%lld", &n);
+    vector<int> a(n);
+    int mi = (int)1e9 + 7;
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &a[i]);
+        mi = min(mi, a[i]);
+    }
+    int ans = 0;
+    int val = 2 * mi - 1;
+    for (int i = 0; i < n; i++) {
+        ans += (a[i]) / val;
+        if (a[i] % val == 0) ans--;
+    }
+    printf("%lld\n", ans);
+}
+
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    for (int tt = 1; tt <= t; tt++) {
+        solve(tt);
+    }
+}
