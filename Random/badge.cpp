a435f4ccab97725306e6a5585f6d592cd27ea10b
Manzood Naqvi <manzood@Manzoods-MacBook-Air.local>
2025-04-13 14:08:55 +0530

    Update filenames and add 2082B

--- Random/badge.cpp
+++ Random/badge.cpp
@@ -1,42 +1,38 @@
-#include <algorithm>
-#include <cstdio>
-#include <iostream>
-#include <set>
-#include <vector>
+#include "bits/stdc++.h"
 
-#include "custom/debugger.h"
+// #include "custom/debugger.h"
 
 using namespace std;
 
 class A {
-   public:
-    int b;
+public:
+  int b;
 
-    int getB() { return b; }
+  int getB() { return b; }
 };
 
 int loop(vector<int> point, int start) {
-    set<int> visited;
-    int current = start;
-    while (visited.count(current) == 0) {
-        visited.insert(current);
-        current = point[current];
-    }
-    return current;
+  set<int> visited;
+  int current = start;
+  while (visited.count(current) == 0) {
+    visited.insert(current);
+    current = point[current];
+  }
+  return current;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> point(n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &point[i]);
-        point[i]--;
-    }
-    int ans;
-    for (int i = 0; i < n; i++) {
-        ans = loop(point, i);
-        printf("%d ", ans + 1);
-    }
-    printf("\n");
+  int n;
+  cin >> n;
+  vector<int> point(n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &point[i]);
+    point[i]--;
+  }
+  int ans;
+  for (int i = 0; i < n; i++) {
+    ans = loop(point, i);
+    printf("%d ", ans + 1);
+  }
+  printf("\n");
 }
